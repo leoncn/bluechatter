e8eda6c578e95013ff1d5353f4b2db6d7db2a969
Leon <leon@localhost.localdomain>
2015-11-11 16:57:54 +0800

    pub & sub

--- rabbit_worker.js
+++ rabbit_worker.js
@@ -4,62 +4,94 @@ var amqp = require('amqp'),
 	os = require('os');
 
 
-if (cluster.isMaster) {
-	//fork workers
-	var numCPUs = os.cpus().length;
+var clients = {};
 
-	for (var i = 0; i < numCPUs; i++) {
-		cluster.fork();
-	}
-
-	cluster.on('online', function(worker) {
-		console.log("worker %s is online.", worker.id);
-	});
-
-	cluster.on('disconnect', function(worker) {
-		console.log("worker %s is disconnect.", worker.id);
-	});
-
-	cluster.on('exit', function(worker) {
-		console.log("worker %s is dead.", worker.id);
-	});
-
-} else {
-	rabbit.on('ready', function() {
-			console.log(' msg Q ready is notified.');
-			var client = Object.create(rabbitSub);
-
-			client.id = 'worker_' + worker.id;
-			client.prototype.onMessage =
-				function(message, headers, deliveryInfo, messageObject) {
-					console.log('%s get a message from %s', this.id, deliveryInfo.queue);
-					console.log("message: %s", JSON.stringify(message));
-				};
-
-
-			client.sub("Q_" + client.id,
-				'first-exchange',
-				'first-queue_rt',
-				client.onMessage.bind(this));
-		});
-
-}
-
-
-
-
-var rabbitSub = {
+var QSubClient = {
 	sub: function sub(clientQueueKey, exchangeKey, routingKey, cb) {
+		var self = this;
 
 		rabbit.queue(clientQueueKey, {
 				autoDelete: false
 			},
 			function(workerQ) {
-				console.log('%s is created.', clientQueueKey);
+				self.queue = workerQ;
+
+				console.log('%s is connected.', clientQueueKey);
 				workerQ.bind(exchangeKey, routingKey, function(q) {
-					console.log('%s is bound.', clientQueueKey);
-					q.subscribe(cb);
+					console.log('%s is bound to %s, routing key : %s', clientQueueKey, exchangeKey, routingKey);
+					q.subscribe({
+						ack: false
+					}, cb).addCallback(function(ok) {
+						self.ctag = ok.consumerTag;
+					});
 				});
 			})
+	},
+
+	unsub: function unsub() {
+		if (this.ctag && this.queue) {
+			this.queue.unsubscribe(this.ctag);
+		}
 	}
+};
+
+var QSubLogClient = Object.create(QSubClient);
+QSubLogClient.onMessage =
+	function(message, headers, deliveryInfo, messageObject) {
+		console.log('%s get a message from %s, curr process : %s.', this.id, deliveryInfo.queue, process.pid);
+		console.log("message: %s", JSON.stringify(message));
+	};
+
+var clients = {};
+
+if (cluster.isMaster) {
+
+	rabbit.on('ready', function() {
+		console.log('connected to MQ successfully.');
+		//fork workers
+		var numCPUs = 1; //os.cpus().length;
+
+		for (var i = 0; i < numCPUs; i++) {
+			cluster.fork();
+		}
+	});
+
+	cluster.on('online', function(worker) {
+		console.log("worker %s is online.", worker.id);
+
+		var id = worker.id;
+		var c = createClient(id);
+		clients[id] = c;
+
+		c.sub("Q_" + c.id,
+			'first-exchange',
+			'first-queue_rt',
+			c.onMessage.bind(c));
+	});
+
+	cluster.on('disconnect', function(worker) {
+		console.log("worker %s is disconnect.", worker.id);
+
+		clients[worker.id].unsub();
+	});
+
+	cluster.on('exit', function(worker) {
+		console.log("worker %s is dead.", worker.id);
+
+		clients[worker.id].unsub();
+		delete clients[worker.id];
+	});
+
+	console.log('connecting to MQ...');
+} else {
+
+}
+
+function createClient(id) {
+	if (!id) return null;
+
+	var c = Object.create(QSubLogClient);
+	c.id = 'worker_' + id;
+
+	return c;
 }
